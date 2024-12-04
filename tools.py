e277753e1fd79bc59391039f5b5c1dc580a4c2e7
czett <keinaniemand@gmail.com>
2024-12-04 23:30:38 +0100

    timer logic

--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
-import requests, json
+import requests, json, time
+from datetime import datetime, timedelta
 
 with open("credentials.yml", "r") as c:
     lines = c.readlines()
@@ -24,3 +25,38 @@ def weather():
 
     return (temperature, rain_probability)
     return {"temp": temperature, "rain_amount": rain_amount, "sun_hrs": sun_hours, "pop": rain_probability, "wind": windspeed, "hum": humidity, "pressure": pressure, "uv": uv_index}
+
+def get_timers() -> list:
+    with open("saves/timers.json", "r") as j:
+        jsonobj = json.load(j)
+    return jsonobj["timers"]
+
+def add_timer(tid: str, dur: int) -> None:
+    timers = get_timers()
+    dt = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    curr_timer = {"timer_id": tid, "duration": dur, "start": dt}
+    timers.append(curr_timer)
+    f = {"timers": timers}
+    with open("saves/timers.json", "w") as t:
+        json.dump(f, t)
+
+def check_timers():
+    completed = []
+    i_compl = []
+    timers = get_timers()
+
+    for index, timer in enumerate(timers):
+        now_plus_dur = datetime.strptime(timer["start"], "%m/%d/%Y, %H:%M:%S") + timedelta(seconds=timer["duration"])
+        if datetime.now() >= now_plus_dur:
+            completed.append(timer)
+            i_compl.append(index)
+
+    i_compl.reverse()
+    for index in i_compl:
+        timers.pop(index)
+
+    f = {"timers": timers}
+    with open("saves/timers.json", "w") as t:
+        json.dump(f, t)
+
+    return completed
