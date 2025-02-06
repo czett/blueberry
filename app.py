760c1a151c6c56bc0149796c703569599392fb74
czett <keinaniemand@gmail.com>
2025-02-06 14:43:38 +0100

    very basic chat bot visualization

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import ollama
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/answer', methods=["POST", "GET"])
+def answer():
+    data = request.get_json()
+    message = data['message']
+
+    def generate():
+        stream = ollama.chat(model="llama3.2:1b", messages=[{"role": "user", "content": message}], stream=True)
+        
+        for chunk in stream:
+            if chunk["message"]["content"] is not None:
+                yield chunk["message"]["content"]
+
+    return generate(), {"Content-Type": "text/plain"}
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5500)
