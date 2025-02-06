68caca879aa6cf723b60fcbc6cc3ca7f413938d9
czett <keinaniemand@gmail.com>
2025-02-06 16:26:06 +0100

    better prefixing and prompt config regardoing web interface

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask, render_template, request
-import ollama
+from flask import Flask, render_template, request, session
+import ollama, json
+
+with open("prompt_config.json", "r") as pc:
+    prefixes = json.load(pc)
+    prompt_prefix = prefixes["standard"]
 
 app = Flask(__name__)
+app.secret_key = "wefhwoeifhaoeiurhgqoeirgh"
+
 
 @app.route('/')
 def index():
+    session["count"] = 0
     return render_template('index.html')
 
 @app.route('/answer', methods=["POST", "GET"])
 def answer():
     data = request.get_json()
-    message = data['message']
+    message = prompt_prefix + data['message']
+    if session["count"] > 1:
+        message = data['message']
+    session["count"] += 1
 
     def generate():
         stream = ollama.chat(model="llama3.2:1b", messages=[{"role": "user", "content": message}], stream=True)
